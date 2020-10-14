9d014e4399d3a9be81b17db64e1d5540db0f0e75
herobrauni <braunnicolaj@gmail.com>
2020-10-14 07:55:32 +0200

    blub

--- sensor/entry.sh
+++ sensor/entry.sh
@@ -2,10 +2,7 @@
 
 mkdir -p /data/sensor
 # As we've now updated, copy the current version here so it matches on next pass
-cp /usr/src/app/version /data/sensor/version
-
-touch /data/sensor/bsec_iaq.state
-cp -n /usr/src/app/bsec_bme680_linux/bsec_iaq.config /data/sensor/bsec_iaq.config
+# cp /usr/src/app/version /data/sensor/version
 
 echo "================ Starting Balena Sense ================"
 
