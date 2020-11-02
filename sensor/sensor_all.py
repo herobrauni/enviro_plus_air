7c126b251c6a16d14d1f2436f7b2db7f617d9e99
herobrauni <braunnicolaj@gmail.com>
2020-11-02 09:53:31 +0100

    Update sensor_all.py

--- sensor/sensor_all.py
+++ sensor/sensor_all.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-import sys
 import time
-import scd30
-import smbus
+import colorsys
 import os
-import json
+import sys
+import scd30
+import ST7735
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
 
-from enviroplus import ENVIROPLUS
+from bme280 import BME280
+from enviroplus import gas
+from subprocess import PIPE, Popen
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from fonts.ttf import RobotoMedium as UserFont
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import logging
+import json
 
 # scd30 init
 PIGPIO_HOST = '::1'
@@ -25,57 +39,40 @@ sensor_scd30.sendCommand(scd30.COMMAND_AUTOMATIC_SELF_CALIBRATION, 1)
 sensor_scd30.waitForDataReady()
 
 # enviro init
-
-
-class sensor():
-    readfrom = 'unset'
-    bus = smbus.SMBus(1)
-
-    def __init__(self):
-        # First, check for enviro plus hat (since it also has BME on 0x76)
-        try:
-            self.bus.write_byte(0x23, 0)  # test if we can connect to ADS1015
-        except IOError:
-            print('Enviro Plus hat not found')
-        else:
-            self.readfrom = 'enviroplus'
-            self.sensor = ENVIROPLUS()
-            print('Found Enviro+ Hat')
-
-        # If this is still unset, no sensors were found; quit!
-        if self.readfrom == 'unset':
-            print('No suitable sensors found! Exiting.')
-            sys.exit()
-
-    def sample(self):
-        return self.sensor.get_readings(self.sensor)
+# BME280 temperature/pressure/humidity sensor
+bme280 = BME280()
 
 
 # Main Loop
 sensor = sensor()
 
+enviro_data = {}
 
 try:
     while True:
+        # get scd30 data
         data = sensor_scd30.readMeasurement()
-
         if (data == False):
             exit(1)
 
             [float_co2, float_T, float_rH] = data
 
-            # if float_co2 > 0.0:
-            #     print("gas_ppm{sensor=\"SCD30\",gas=\"CO2\"} %f" % float_co2)
+        # get enviro data
 
-            # print("temperature_degC{sensor=\"SCD30\"} %f" % float_T)
+        enviro_data.proximity = ltr559.get_proximity()
+        enviro_data.raw_temp = bme280.get_temperature()
+        enviro_data.pressure = bme280.get_pressure()
+        enviro_data.humidity = bme280.get_humidity()
+        enviro_data.light = ltr559.get_lux()
+        enviro_gas = gas.read_all()
+        enviro_data.ox = enviro_gas.oxidising / 1000
+        enviro_data.red = enviro_gas.reducing / 1000
+        enviro_data.nh3 = enviro_gas.nh3 / 1000
 
-            # if float_rH > 0.0:
-            #     print("humidity_rel_percent{sensor=\"SCD30\"} %f" % float_rH)
-
-        measurements = sensor.sample()
-        print(measurements)
-        print("co2: " + float_co2 + " temp: " + float_T + " hum: " + float_rH)
-        time.sleep(3)
+        # Outputs
+        print("temp: " + float_T + " co2: " + float_co2 + " hum: " + float_rH)
+        print(enviro_data)
+        time.sleep(10)
 
 
 except KeyboardInterrupt:
